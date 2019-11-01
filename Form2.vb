5f5737fac16ac88750442a4c94ddfe03d891ccdf
守真 孫 <oscarsun72@hotmail.com>
2019-11-01 22:34:27 +0800

    加入專案檔案。

---
+++ Form2.vb
@@ -0,0 +1,14 @@
+﻿Public Class Form2
+    Private Sub Form2_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim frm1 = Application.OpenForms("Form1")
+        'frm1.Close() '不能關掉，因為是整個應用程式main()的入口，關掉Form1就等於結束應用程式了
+        '除非在應用程式屬性中另外指派一個main()入口的物件（即在應用程式屬性中「啟動表單」的選項，不能設為Form1）
+        '或在程式關閉模式，不要選擇「啟動表單關閉時」這個選項
+        frm1.Visible = False
+    End Sub
+
+    Private Sub Form2_FormClosing(sender As Object, e As FormClosingEventArgs) Handles MyBase.FormClosing
+        Dim frm1 = Application.OpenForms("Form1")
+        frm1.Visible = True
+    End Sub
+End Class
